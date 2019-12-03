0168d68de102f3431f920199ac327ac3898f4170
Sille Van Landschoot <info@sillevl.be>
2019-12-03 11:21:10 +0100

    use axios to fetch data from api

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <div class="container">
     <h1>My todo app</h1>
     <ul>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoItem
+        v-for="item in todoItems"
+        v-bind:key="item.id"
+        v-bind:title="item.title"
+        v-bind:state="item.state"
+      />
     </ul>
   </div>
 </template>
@@ -12,10 +15,27 @@
 <script>
 
 import TodoItem from './components/TodoItem'
+import axios from 'axios'
 
 export default {
   components: {
     TodoItem
+  },
+  data: function () {
+    return {
+      todoItems: []
+    }
+  },
+  mounted: function () {
+    this.getTodoItems()
+  },
+  methods: {
+    getTodoItems: function () {
+      axios('http://localhost:3000/todoitem')
+        .then(reponse => {
+          this.todoItems = reponse.data
+        })
+    }
   }
 }
 </script>
