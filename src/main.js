aeb82361b2e4166a8ec65c3710eedd6173eb7e71
Sille Van Landschoot <info@sillevl.be>
2019-12-03 10:50:32 +0100

    add Materializecss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-multi-spaces */
 import Vue from 'vue'
 import App from './App.vue'
+
+import 'materialize-css'                            // imports materialize javascript
+import 'materialize-css/dist/css/materialize.css'   // imports materialize css
 
 Vue.config.productionTip = false
 
