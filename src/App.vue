39b457a22da4c2da6f492fea81f6b19c1d9401e3
Sille Van Landschoot <info@sillevl.be>
2019-12-03 10:50:45 +0100

    Add TodoItem component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div >
+  <div class="container">
     <h1>My todo app</h1>
+    <ul>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </ul>
   </div>
 </template>
 
 <script>
 
-export default {
+import TodoItem from './components/TodoItem'
 
+export default {
+  components: {
+    TodoItem
+  }
 }
 </script>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <li class="card blue-grey white-text">
+        <div class="row">
+            <div class="col s10"> {{ title }}</div>
+            <div class="col s2"> {{ state }}</div>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'title',
+      state: 'state'
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  padding: 10px;
+}
+
+div.row {
+  margin-bottom: 0px;
+}
+</style>
